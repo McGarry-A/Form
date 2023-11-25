a20eb98ddbf1101ac55108e2d6ef048df4099f92
AMG <Other@Ahmeds-MacBook-Pro.local>
2023-11-25 13:30:33 +0000

    set up storybook and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "bg-color": "#EFF5FF",
+        "border-color": "#D6D9E6",
+        "denim": "#022959",
+        "grey": "#9699AA",
+        "light-blue": "#ABBCFF",
+        "light-grey": "#D6D9E6",
+        "orange": "#FFAF7E",
+        "pink": "#F9818E",
+        "purple": "#483EFF",
+        "red-error": "#EE374A",
+        "sky-blue": "#BEE2FD",
+        "very-light-grey": "#F8F9FF",
+        "white": "#FFFFFF",
+      },
+      fontSize: {
+        "sm": "12px",
+        "md": "14px",
+        "lg": "16px",
+        "xl": "32px"
+      },
+    },
+  },
+  plugins: [],
+}
+
